5096a1dba849fe6c8bb02c4f4b620b5514bcbbdd
ebo16@vt.edu <ebo16@vt.edu>
2021-10-07 20:37:33 -0400

    class and framework

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+// fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+// .then(Response => Response.json())
+// .then (data => {
+//     var mealTitle = data.meals.strMeal;
+
+//     mealTitle.innerHTML = mealTitle;
+// })
+
