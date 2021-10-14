0baa0e114bd47aaeb8ff7210fa0fe8af785f08ed
ebo16@vt.edu <ebo16@vt.edu>
2021-10-13 20:28:37 -0400

    add drink instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,34 +9,6 @@ var mealInstructions = document.querySelector('.mealInstructions');
 var mealIngredrients = document.querySelector('.mealIngredients')
 
 
-// function mealFunction() {
-//   fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-//     .then(res => res.json())
-//     .then(res => {
-//       createMeal(res.meals[0])
-
-//     })
-  // function mealFunction () {  
-  //     fetch ('https://www.themealdb.com/api/json/v1/1/random.php')
-
-  //       .then(function(response) {
-  //       return response.json();
-  //       })
-  //       .then(function(response) {
-  //       console.log(response);
-
-  //     var displayMealContainerEl = document.querySelector("#mealBoxID");
-
-  //       //empty div before appending image
-  //       displayMealContainerEl.innerHTML="";
-
-  //     var mealImg = document.createElement('img');
-  //     mealImg.setAttribute('src', response.strMealThumb);
-
-  //     displayMealContainerEl.appendChild(mealImg);
-
-  //     });}
-
   button.addEventListener('click', function (event) {
     event.preventDefault();
     fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + searchValue.value)
@@ -52,14 +24,8 @@ var mealIngredrients = document.querySelector('.mealIngredients')
         console.log(n);
         var drinkRandom = data.drinks[n].strDrink;
 
-        console.log(drinkRandom)
-
-        // let drinkRandom = drinkApiArray[Math.floor(Math.random()*drinkApiArray.length)].strDrink;
-        // console.log(drinkRandom);
-
-
         //displays drink data on the app
-        cocktailName.innerHTML = "Your Cocktail is a: " + drinkRandom;
+        cocktailName.innerHTML = drinkRandom;
         
         var drinkDivEl = document.querySelector('#drink-container');
         drinkDivEl.innerHTML = "";
@@ -67,15 +33,9 @@ var mealIngredrients = document.querySelector('.mealIngredients')
         drinkImg.setAttribute('src', data.drinks[n].strDrinkThumb);
         drinkDivEl.appendChild(drinkImg)
 
+        findDrinkRecipe(drinkRandom);
       })
 
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + cocktailName)
-      .then(Response => Response.json())
-      .then(data => {
-        console.log(cocktailName)
-        var drinkInstructions = (data.drinks.strInstructions);
-        console.log(drinkInstructions);
-      })
 
    
    //fetch random meal and display
@@ -89,7 +49,6 @@ var mealIngredrients = document.querySelector('.mealIngredients')
         //pull information from API
       var mealNameValue = (data.meals[0].strMeal);
       console.log(mealNameValue);
-
 
       var displayMealContainerEl = document.querySelector("#meal-container");
 
@@ -105,42 +64,32 @@ var mealIngredrients = document.querySelector('.mealIngredients')
       var mealInstructionsValue = (data.meals[0].strInstructions);
       
       //display info from API on screen
-      mealName.innerHTML = "Your Meal is: " + mealNameValue;
-      mealInstructions.innerHTML = mealInstructionsValue;
+      mealName.innerHTML =  mealNameValue;
+      mealInstructions.innerHTML = "Instructions: " + mealInstructionsValue;
 
   })
+
+  function findDrinkRecipe(drinkRandom) {
+    drinkRandom = drinkRandom.split(" ").join("+").toLowerCase();
+    var requestOptions = {
+      method: "GET",
+      headers: "myHeaders",
+      redirect: "follow",
+    }
+    console.log(drinkRandom);
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drinkRandom, requestOptions)
+    .then(response => {
+      console.log(response)
+      response.json()
+    }) 
+    .then(data => {
+      console.log(data)
+      var drinkInstructions = (data.drinks[0].strInstructions);
+      console.log(drinkInstructions);
+    })
+  }
+
+
 });
 
-
-  // function drinkFunction () {
-
-  //         fetch ('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i='+searchValue.value)
-
-  //           .then(function(response) {
-  //           return response.json();
-  //           })
-  //           .then(function(response) {
-  //           console.log(response);
-  //           console.log(searchValue.value);
-  //         //select one response from Drink API array 
-  //         let drinkApiArray = response;
-  //         let drink = drinkApiArray[Math.floor(Math.random()*drinkApiArray.length)];
-  //         console.log(drink);
-
-  //         // Create a variable that will select the <div> where the GIF will be displayed
-  //         // YOUR CODE HERE
-  //           var displayDrinkContainerEl = document.querySelector("#drinkBoxID");
-
-  //         // Empty out the <div> before we append a GIF to it
-  //         // YOUR CODE HERE
-  //           displayDrinkContainerEl.innerHTML="";
-
-  //         var drinkImg = document.createElement('img');
-  //         drinkImg.setAttribute('src', response.strDrinkThumb);
-  //         // Append 'gifImg' to the <div>
-  //         // YOUR CODE HERE
-  //         //
-  //         displayDrinkContainerEl.appendChild(drinkImg);
-
-  //         });}
 
